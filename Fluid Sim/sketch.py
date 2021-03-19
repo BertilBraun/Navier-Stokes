51113150ae38c9b40431538157182ae6c6988e15
OrtwIn <bertil.braun.private@gmail.com>
2021-03-20 00:19:16 +0100

    Brython website implementation...
    Doesn't work since Brython doesn't support numpy nor numba

--- Fluid Sim/sketch.py
+++ Fluid Sim/sketch.py
@@ -1,51 +1,53 @@
 
 from fluid import Fluid
 from settings import N, scale, density_scale, vel_scale
-from timeme import timeme
 
-import pygame
-pygame.init()
+from browser import document, html, timer
 
 fluid = Fluid()
-screen = pygame.display.set_mode([scale * N, scale * N])
 
 last_x, last_y = 0, 0
 
-@timeme
+canvas = html.CANVAS(width=scale * N, height=scale * N)
+ctx = canvas.getContext("2d")
+
+document["canvas"] <= canvas
+
+
 def update():
-    global last_x, last_y
-
-    x, y = pygame.mouse.get_pos()
-    if pygame.mouse.get_pressed()[0]:
-        fluid.addDensity(x // scale, y // scale, density_scale)
-        fluid.addVelocity(x // scale, y // scale, (last_x - x) * vel_scale, (last_y - y) * vel_scale)
-
-    last_x = x
-    last_y = y
     fluid.update()
 
 
 def draw():
-    screen.fill((255, 255, 255))
     for i in range(N):
         for j in range(N):
             c = min(255, max(fluid.density[i, j] * 1000, 0))
             c = min(255, max(fluid.s[i, j] * 1000, 0))
-            screen.fill((c, c, c), ((i * scale, j * scale), (scale, scale)))
+            ctx.fillStyle = "rgb("+ c + ","+ c + ","+ c + ")"
+            ctx.fillRect(i * scale, j * scale, scale, scale)
 
 
-def main():
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit(0)
+def canvasClick(ev):
+    global last_x, last_y
 
-        update()
-        draw()
+    rect = canvas.getBoundingClientRect()
+    x = (ev.x - rect.left) // scale
+    y = (ev.y - rect.top) // scale
 
-        pygame.display.flip()
+    if x < 0 or x >= N or y < 0 or y >= N:
+        return
+
+    fluid.addDensity(x, y, density_scale)
+    fluid.addVelocity(x, y, (last_x - x) * vel_scale, (last_y - y) * vel_scale)
+
+    last_x = x
+    last_y = y
 
 
-if __name__ == "__main__":
-    main()
+document["canvas"].bind('click', canvasClick)
+
+while True:
+
+    update()
+    draw()
+
